2bab4dbc573ebe0f0c3083e8e1edb432b70e2b90
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-09-06 02:20:42 +0500

    connected with mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,20 @@
 import express from 'express'
 import 'express-async-errors'
 import {json} from 'body-parser'
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-import {currentUserRouter} from "./current-user"
-import {signinRouter} from "./signin"
-import {signoutRouter} from "./signout"
-import {signupRouter} from "./signup"
+import {currentUserRouter} from './current-user'
+import {signinRouter} from './signin'
+import {signoutRouter} from './signout'
+import {signupRouter} from './signup'
 
-import {errorHandler} from "./middlewares/error-handler";
-import {NotFoundError} from "./errors/not-found-error";
+import {errorHandler} from './middlewares/error-handler'
+import {NotFoundError} from './errors/not-found-error'
 
 const app = express()
 
 // app.all('*', async () => {
-//     throw new NotFoundError();
+//     throw new NotFoundError()
 // })
 
 // no need to call async with next due to use of express-async-errors now we can directly throw errors from async func as well
@@ -29,8 +29,16 @@ app.use(signoutRouter)
 app.use(signupRouter)
 app.use(errorHandler)
 
-const port = 3000
-
-app.listen(port, () => {
-    console.log(`Auth Service Listening On Port : ${port}`)
-})
+const start = async () => { //
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to mongodb.')
+    }catch (e) {
+        console.error(e)
+    }
+    const port = 3000
+    app.listen(port, () => {
+        console.log(`Auth Service Listening On Port : ${port}`)
+    })
+}
+start()
