93219ba55cc9fce4fcd049edf4e83b7ef77aa0dc
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-09-08 03:24:17 +0500

    commented code which is not in use now testing part will start

--- auth/src/current-user.ts
+++ auth/src/current-user.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 import {currentUser} from "./middlewares/current-user";
-import {requireAuth} from "./middlewares/require-auth";
+// import {requireAuth} from "./middlewares/require-auth";
 const router = express.Router()
 
 // router.get('/api/users/currentuser', currentUser, requireAuth, (req: Request, res: Response) => {
