fbd95e39f0d1aedc6a10bc05d1b21f5d2a9403f9
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-09-08 00:47:09 +0500

    jwt key as a secret has been passed from kubernetes create secret method

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,7 +34,9 @@ app.use(signoutRouter)
 app.use(signupRouter)
 app.use(errorHandler)
 
-const start = async () => { //
+const start = async () => {
+    if(!process.env.JWT_KEY)
+        throw new Error('JWT_KEY must be defined.')
     try{
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
         console.log('Connected to mongodb.')
--- auth/src/signup.ts
+++ auth/src/signup.ts
@@ -37,7 +37,7 @@ router.post('/api/users/signup', [
     const userJwt = jwt.sign({
         id: user.id,
         email: user.email,
-    }, 'aaa')
+    }, process.env.JWT_KEY!)
     // store it on session object
     req.session = {
         jwt: userJwt
