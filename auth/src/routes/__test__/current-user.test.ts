439d9f60710d8868627927dddcaf4a03d8102344
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-09-09 20:01:17 +0500

    current user functionality tested as well

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import {app} from '../../app'
+
+it('responds with details about the current user', async () => {
+    const email = 'validemail@domain.com'
+    const pass = 'aA213sdf$sdf'
+    const authResponse = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: email,
+            password: pass,
+        })
+        .expect(201)
+
+    const cookie = authResponse.get('Set-Cookie')
+
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .set('Cookie', cookie)
+        .send({})
+        .expect(200)
+
+    expect(response.body.currentUser.email).toEqual(email)
+})
